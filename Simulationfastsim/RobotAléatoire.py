e85a6d2e4777fa195be00cd5f100e6759772e457
Yurof <48862755+Yurof@users.noreply.github.com>
2021-01-26 16:12:51 +0100

    -

--- Simulationfastsim/RobotAléatoire.py
+++ Simulationfastsim/RobotAléatoire.py
@@ -1,58 +1,56 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-
 import gym, gym_fastsim
 import time
 import random
+import sys
 
 
-display= True
+display = True
+time_sleep= 0.01
 
 env = gym.make('kitchen-v0')
 env.reset()
-action=[10,11]
 
 if(display):
 	env.enable_display()
 
 then = time.time()
-i=0
-o,r,eo,info=env.step(action)
-print("Step %d Obs=%s  reward=%f  dist. to objective=%f  robot position=%s  End of ep=%s" % (i, str(o), r, info["dist_obj"], str(info["robot_pos"]), str(eo)))
-print(env.robot.get_collision())
+i = 0 
+o, r, eo, info = env.step([0,0])
+diametre_goal= env.goal.get_diam()
+
+def mouvement(l, r, n):
+    for k in range(n):
+        o, r, eo, info = env.step([l,r])
+        if(display):
+            time.sleep(time_sleep)
+        if eo:
+            break
+        env.render()
+        global i
+        i+= 1
+        print("Step %d Obs=%s reward=%f  dist. to objective=%f  robot position=%s" % (i, str([round(num,1) for num in o]),r, info["dist_obj"], str(info["robot_pos"]) ) , end="\r" )
 
 
-while info["dist_obj"]>10 :
-        
-    env.render()
-    o,r,eo,info=env.step([random.randint(-2,5),random.randint(-2,5)])
-
-    if env.robot.get_left_bumper()==True and env.robot.get_right_bumper()==True:
-        print("bumber gauche et droit ")
+while info["dist_obj"]>diametre_goal :
+    if env.robot.get_left_bumper() and env.robot.get_right_bumper():
+        #print("bumber gauche et droit ")
         env.robot.reinit()
-        o,r,eo,info=env.step([-10, -10 ])
-        o,r,eo,info=env.step([-2, 2 ])
-    
-    elif env.robot.get_left_bumper()==True:
-        print("bumber gauche")
-        env.robot.reinit()
-        o,r,eo,info=env.step([-2, 2 ])
-        o,r,eo,info=env.step([5, 5 ])
+        mouvement(-2, 2, 20)
 
-    elif env.robot.get_right_bumper()==True:
-        print("bumber droit")
+    elif env.robot.get_left_bumper():
+        #print("bumber gauche")
         env.robot.reinit()
-        o,r,eo,info=env.step([2, -2 ])
-        o,r,eo,info=env.step([5, 5 ])
+        mouvement(-2, 2, 1)
 
-    print("Step %d Obs=%s  reward=%f  dist. to objective=%f  robot position=%s  End of ep=%s" % (i, str(o), r, info["dist_obj"], str(info["robot_pos"]), str(eo)))
-    
-    if(display):
-        time.sleep(0.01)
-    if eo:
-        break
-    i+=1
+    elif env.robot.get_right_bumper():
+        #print("bumber droit")
+        env.robot.reinit()
+        mouvement(2, -2, 1)
+
+    else:
+        mouvement(random.randint(-2,5),random.randint(-2,5),1)
 
 
 now = time.time()
