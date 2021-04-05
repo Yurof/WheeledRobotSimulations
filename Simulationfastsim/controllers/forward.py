82c33aea6c904d39570ae1544f864d4109c46bac
Denis Zheng <denn.zheng@gmail.com>
2021-04-05 11:10:17 +0200

    added fastsim controllers and fixes

---
+++ Simulationfastsim/controllers/forward.py
@@ -0,0 +1,92 @@
+import random
+
+class ForwardController:
+    """
+    With this controller, the agent will always go forward when possible,
+    otherwise it will react accordingly to the obstacle or wall ahead
+    """
+
+    def __init__(self, env, verbose=False):
+        
+        self.env = env
+        self.verbose = verbose
+        
+        # behavioral parameters
+        self.dist_tooClose = 30
+        self.wall_tooCloseF = False
+        self.wall_tooCloseR = False
+        self.wall_tooCloseL = False
+        self.right = [-1, 1]
+        self.left = [1, -1]
+        self.forward = [1, 1]
+        
+        # there is this case where the agent might be stuck and alternate 
+        # endlessly between left and right, so we add an additional parameter 
+        # to try to prevent that
+        self.old_c = self.forward
+        
+    
+    def get_command(self):
+        
+        # command to return at the end of this function
+        c = self.forward
+        
+        # get lasers data
+        laserRanges = self.env.get_laserranges()
+        
+        for i in range(len(laserRanges)):
+            if laserRanges[i] < self.dist_tooClose:
+                # assuming there are 4 radars on the left, 2 on the front,
+                # 4 on the right
+                if i in range(4):
+                    self.wall_tooCloseL = True
+                if i in range(4,6):
+                    self.wall_tooCloseF = True                    
+                if i in range(6,10):
+                    self.wall_tooCloseR = True
+        
+        # get bumpers data
+        bumpers = self.env.get_bumpers()   
+                 
+        # we adapt our policy based on what we have detected
+        if self.wall_tooCloseF:
+            if self.verbose:
+                print("WALL F")
+            # randomly turn right or left
+            if random.random() < 0.5:
+                c = self.right
+            else:
+                c = self.left
+                
+        elif bumpers[0] or self.wall_tooCloseL:
+            if self.verbose:
+                print("WALL L")
+            # turn right
+            c = self.right
+            
+        elif bumpers[1] or self.wall_tooCloseR:
+            if self.verbose:
+                print("WALL R")
+            #turn left
+            c = self.left
+        
+        if (c == self.right and self.old_c == self.left) or \
+        (c == self.left and self.old_c == self.right):
+            c = self.old_c
+        self.old_c = c
+        
+        if self.verbose:
+            print(f"Chosen action : {c}")
+            
+        return c
+        
+        
+    def reset(self):
+        self.dist_tooClose = 40
+        self.wall_tooCloseF = False
+        self.wall_tooCloseR = False
+        self.wall_tooCloseL = False
+        self.right = [-1, 1]
+        self.left = [1, -1]
+        self.forward = [1, 1]
+        
--- Simulationfastsim/gym_fastsim/simple_nav/nav_env.py
+++ Simulationfastsim/gym_fastsim/simple_nav/nav_env.py
@@ -1,4 +1,7 @@
-import os, subprocess, time, signal
+import os
+import subprocess
+import time
+import signal
 import numpy as np
 import gym
 from gym import error, spaces
@@ -15,51 +18,55 @@ logger = logging.getLogger(__name__)
 # default_env = "assets/LS_maze_hard.xml"
 default_env = "assets/example.xml"
 
-sticky_walls = False # default is True in libfastsim... but false in the fastsim sferes2 module -> stick (haha) with that
+# default is True in libfastsim... but false in the fastsim sferes2 module -> stick (haha) with that
+sticky_walls = False
 
 
-def sqdist(x,y):
-	return (x[0]-y[0])**2+(x[1]-y[1])**2
+def sqdist(x, y):
+    return (x[0]-y[0])**2+(x[1]-y[1])**2
 
-def dist(x,y):
-	return math.sqrt(sqdist(x,y))
+
+def dist(x, y):
+    return math.sqrt(sqdist(x, y))
 
 
 def reward_binary_goal_based(navenv):
-	""" Reward of 1 is given when close enough to the goal. """
-	curpos = navenv.current_pos
-	if (dist(curpos,navenv.goalPos)<=navenv.goalRadius):
-		return 1.
-	else:
-		return 0.
+    """ Reward of 1 is given when close enough to the goal. """
+    curpos = navenv.current_pos
+    if (dist(curpos, navenv.goalPos) <= navenv.goalRadius):
+        return 1.
+    else:
+        return 0.
+
 
 def reward_minus_energy(navenv):
-	""" Reward = minus sum of absolute values of motor orders"""
-	r = -(np.abs(navenv.v1_motor_order) + np.abs(navenv.v2_motor_order))
-	return r
+    """ Reward = minus sum of absolute values of motor orders"""
+    r = -(np.abs(navenv.v1_motor_order) + np.abs(navenv.v2_motor_order))
+    return r
+
 
 def reward_displacement(navenv):
-	""" Reward = distance to previous position"""
-	r = dist(navenv.current_pos, navenv.old_pos)
-	return r
+    """ Reward = distance to previous position"""
+    r = dist(navenv.current_pos, navenv.old_pos)
+    return r
+
 
 def no_reward(navenv):
-	""" No reward"""
-	return 0.
+    """ No reward"""
+    return 0.
 
 
-reward_functions = { "binary_goalbased":reward_binary_goal_based,
-				"minimize_energy":reward_minus_energy,
-				"displacement":reward_displacement,
-				"none":no_reward,
-				None:no_reward}
-
+reward_functions = {"binary_goalbased": reward_binary_goal_based,
+                    "minimize_energy": reward_minus_energy,
+                    "displacement": reward_displacement,
+                    "none": no_reward,
+                    None: no_reward}
 
 
 class SimpleNavEnv(gym.Env):
-    def __init__(self,xml_env, reward_func="binary_goalbased",render=False, light_sensor_range=200., light_sensor_mode="realistic"):
-		# Fastsim setup
-		# XML files typically contain relative names (for map) wrt their own path. Make that work
+    def __init__(self, xml_env, reward_func="binary_goalbased", render=False, light_sensor_range=200., light_sensor_mode="realistic"):
+        # Fastsim setup
+        # XML files typically contain relative names (for map) wrt their own path. Make that work
         xml_dir, xml_file = os.path.split(xml_env)
         if(xml_dir == ""):
             xml_dir = "./"
@@ -68,56 +75,57 @@ class SimpleNavEnv(gym.Env):
         settings = fs.Settings(xml_file)
         os.chdir(oldcwd)
         print("test")
-		
 
         self.map = settings.map()
         self.robot = settings.robot()
-        
+
         if(render):
             self.display = fs.Display(self.map, self.robot)
         else:
             self.display = None
-		
-        self.maxVel = 4 # Same as in the C++ sferes2 experiment
-		
-		# Lasers
+
+        self.maxVel = 4  # Same as in the C++ sferes2 experiment
+
+        # Lasers
         lasers = self.robot.get_lasers()
         n_lasers = len(lasers)
         if(n_lasers > 0):
-            self.maxSensorRange = lasers[0].get_range() # Assume at least 1 laser ranger
+            # Assume at least 1 laser ranger
+            self.maxSensorRange = lasers[0].get_range()
         else:
             self.maxSensorRange = 0.
-		
-		#Light sensors
+
+        # Light sensors
         self.ls_mode = light_sensor_mode
         lightsensors = self.robot.get_light_sensors()
         n_lightsensors = len(lightsensors)
         if(n_lightsensors > 0):
-            self.maxLightSensorRange = light_sensor_range # Assume at least 1 laser ranger
+            self.maxLightSensorRange = light_sensor_range  # Assume at least 1 laser ranger
         else:
             self.maxLightSensorRange = 0.
-		
-		# State
+
+        # State
         self.initPos = self.get_robot_pos()
         self.current_pos = self.get_robot_pos()
         self.old_pos = self.get_robot_pos()
 
         self.v1_motor_order = 0.
         self.v2_motor_order = 0.
-		
-        self.goal = self.map.get_goals()[0] # Assume 1 goal
-        self.goalPos=[self.goal.get_x(),self.goal.get_y()]
+
+        self.goal = self.map.get_goals()[0]  # Assume 1 goal
+        self.goalPos = [self.goal.get_x(), self.goal.get_y()]
         self.goalRadius = self.goal.get_diam()/2.
-		
-        self.observation_space = spaces.Box(low=np.array([0.]*n_lasers + [0.]*2 + [0. if (self.ls_mode == "realistic") else -1]*n_lightsensors), high=np.array([self.maxSensorRange]*n_lasers + [1. if (self.ls_mode == "realistic") else self.maxLightSensorRange]*2 + [1.]*n_lightsensors, dtype=np.float32))
-        self.action_space = spaces.Box(low=-self.maxVel, high=self.maxVel, shape=(2,), dtype=np.float32)
-		
-		# Reward
+
+        self.observation_space = spaces.Box(low=np.array([0.]*n_lasers + [0.]*2 + [0. if (self.ls_mode == "realistic") else -1]*n_lightsensors), high=np.array(
+            [self.maxSensorRange]*n_lasers + [1. if (self.ls_mode == "realistic") else self.maxLightSensorRange]*2 + [1.]*n_lightsensors, dtype=np.float32))
+        self.action_space = spaces.Box(
+            low=-self.maxVel, high=self.maxVel, shape=(2,), dtype=np.float32)
+
+        # Reward
         if(reward_func not in reward_functions):
             raise RuntimeError("Unknown reward '%s'" % str(reward_func))
         else:
             self.reward_func = reward_functions[reward_func]
-		
 
     def enable_display(self):
         if not self.display:
@@ -145,7 +153,7 @@ class SimpleNavEnv(gym.Env):
             if r < 0:
                 out.append(self.maxSensorRange)
             else:
-                out.append(np.clip(r,0.,self.maxSensorRange))
+                out.append(np.clip(r, 0., self.maxSensorRange))
         return out
 
     def get_lightsensors(self):
@@ -153,11 +161,12 @@ class SimpleNavEnv(gym.Env):
         for ls in self.robot.get_light_sensors():
             r = ls.get_distance()
             if(self.ls_mode == "realistic"):
-				# r is -1 if no light, dist if light detected 
-				# We want the output to be a "realistic" light sensor :
-				# - 0 if no light (no target in field or out of range)
-				# - (1-d/maxRange)**2 if light detectted
-                if((r < 0) or (r > self.maxLightSensorRange)): # Out of range, or not in angular range
+                # r is -1 if no light, dist if light detected
+                # We want the output to be a "realistic" light sensor :
+                # - 0 if no light (no target in field or out of range)
+                # - (1-d/maxRange)**2 if light detectted
+                # Out of range, or not in angular range
+                if((r < 0) or (r > self.maxLightSensorRange)):
                     out.append(0.)
                 else:
                     out.append((1. - r/self.maxLightSensorRange)**2)
@@ -168,45 +177,43 @@ class SimpleNavEnv(gym.Env):
 
         return out
 
-
     def get_bumpers(self):
         return [float(self.robot.get_left_bumper()), float(self.robot.get_right_bumper())]
-
 
     def get_all_sensors(self):
         return self.get_laserranges() + self.get_bumpers() + self.get_lightsensors()
 
     def step(self, action):
-		# Action is: [leftWheelVel, rightWheelVel]
+        # Action is: [leftWheelVel, rightWheelVel]
         [v1, v2] = action
-		
-        self.v1_motor_order = np.clip(v1,-self.maxVel,self.maxVel)
-        self.v2_motor_order = np.clip(v2,-self.maxVel,self.maxVel)
-		
-        self.robot.move(self.v1_motor_order, self.v2_motor_order, self.map, sticky_walls)
+
+        self.v1_motor_order = np.clip(v1, -self.maxVel, self.maxVel)
+        self.v2_motor_order = np.clip(v2, -self.maxVel, self.maxVel)
+
+        self.robot.move(self.v1_motor_order,
+                        self.v2_motor_order, self.map, sticky_walls)
 
         sensors = self.get_all_sensors()
         reward = self._get_reward()
-		
+
         self.old_pos = self.current_pos
         self.current_pos = self.get_robot_pos()
-		
-		#if(sqdist(p,self.roldpos)<0.001**2):
-		#	self.still=self.still+1
-		#else:
-		#	self.still=0
-		#self.roldpos=p
-		#episode_over = self.still>=self.still_limit
-        episode_over = False
+
+        # if(sqdist(p,self.roldpos)<0.001**2):
+        #	self.still=self.still+1
+        # else:
+        #	self.still=0
+        # self.roldpos=p
+        #episode_over = self.still>=self.still_limit
 
         dist_obj = dist(self.current_pos, self.goalPos)
+        episode_over = dist_obj <= self.goal.get_diam()
 
-        return sensors, reward, episode_over, {"dist_obj":dist_obj, "robot_pos":self.current_pos}
-
+        return sensors, reward, episode_over, {"dist_obj": dist_obj, "robot_pos": self.current_pos}
 
     def _get_reward(self):
-        return self.reward_func(self) # Use reward extraction function
-		
+        return self.reward_func(self)  # Use reward extraction function
+
     def reset(self):
         p = fs.Posture(*self.initPos)
         self.robot.set_pos(p)
